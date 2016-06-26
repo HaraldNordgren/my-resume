aa968c259c820396bba71ea8873f79889c7b3ad1
HaraldNordgren <haraldnordgren@gmail.com>
2016-06-27 01:14:36 +0200

    Upated plaintext script

--- CV/English/letter-to-plaintext.sh
+++ CV/English/letter-to-plaintext.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+set -e
+
 out=personal-letter-plaintext.txt
 
 echo -e "Dear Hiring Manager,\n" > $out
 detex personal-letter.tex >> $out
 echo -e "\n\nSincerely,\nHarald Nordgren" >> $out
 
-
+echo; echo ¤¤ Converted personal letter to plaintext ¤¤
